261351cec039c3c18b8915364b49e139e0705cf7
franSborges <borgesfran988@gmail.com>
2023-01-18 14:29:41 -0300

    getters and setters created

---
+++ src/app/entities/notification.ts
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createAt(): Date {
+    return this.props.createdAt;
+  }
+
+}
+
--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,9 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'crypto';
-import { CreateNotificationBody } from './create-notification-body';
+import { CreateNotificationBody } from '../infra/create-notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -16,7 +15,7 @@ export class AppController {
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
-   const { recipientId, content, category } = body;
+    const { recipientId, content, category } = body;
 
     await this.prisma.notification.create({
       data: {
