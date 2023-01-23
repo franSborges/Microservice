00fbeb484a7e6bdacd636c7b3cdef6f6b68bc934
franSborges <borgesfran988@gmail.com>
2023-01-23 10:45:05 -0300

    :feat: tests for notification done

---
+++ src/app/entities/notification.spec.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Content } from "./content";
+import { Notification } from "./notification";
+
+
+describe('Notification', () => {
+ it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notification).toBeTruthy();
+  });
+
+});
